cf3331eb7c80a76d19edada73ffcbc260e84ff34
maspaitujaki <13520156@std.stei.itb.ac.id>
2023-03-05 12:32:10 +0700

    initial commit

---
+++ src/pages/homePage.js
@@ -0,0 +1,10 @@
+import './homePage.css'
+export const HomePage = () =>{
+    return(
+        <div>
+            <div className='title'>
+                <h1>Pairing System</h1>
+            </div>
+        </div>
+    )
+}
