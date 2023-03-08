d7521a0a4ab4b2cdda92b75c887be17e538aa61b
maspaitujaki <13520156@std.stei.itb.ac.id>
2023-03-09 01:33:11 +0700

    Feat: Add players UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
 import './App.css';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import { HomePage } from './pages/homePage';
+import { Button, ChakraProvider, Text, HStack, Stack, Divider, Flex, Spacer, VStack, Input} from '@chakra-ui/react';
 function App() {
   return (
-      <HashRouter>
-        <Routes>
-          <Route path='/' element={<HomePage/>}/>
-        </Routes>
-      </HashRouter>
+    <ChakraProvider>
+      <HStack spacing='5' w='95vw' mx={10} my={10}>
+        <VStack maxW='25vw' alignSelf='flex-start'>
+          <Stack p={2} border='1px' borderColor='gray.400' borderRadius='md'>
+            <Button whiteSpace="normal" height="auto" blockSize="auto">
+              <Text padding={2}>New Tournament</Text>
+            </Button>
+            <Divider/>
+            <Flex>
+              <Text>Ongoing</Text>
+              <Spacer/>
+              <Text fontSize='sm' bg='gray.200'px={2} borderRadius='sm' color='gray.800'>1</Text>
+            </Flex>
+            <Divider/>
+            <Flex>
+              <Text>Created</Text>
+              <Spacer/>
+              <Text fontSize='sm' bg='gray.200'px={2} borderRadius='sm' color='gray.800'>1</Text>
+            </Flex>
+            <Divider/>
+            <Flex>
+              <Text>Finished</Text>
+              <Spacer/>
+              <Text fontSize='sm' bg='gray.200'px={2} borderRadius='sm' color='gray.800'>1</Text>
+            </Flex>
+            <Divider/>
+            <Input type='text' placeholder='Search...'/>
+          </Stack>
+          <Stack w='full' p={2} border='1px' borderColor='gray.400' borderRadius='md'> 
+            <Text>
+              Players
+            </Text>
+          </Stack>
+        </VStack>
+        <Stack id="content" w='60vw' minH='20vw'px={2} py={2} border='1px' borderColor='gray.400' borderRadius='md'>
+          <HashRouter>
+            <Routes>
+              <Route path='/' element={<HomePage/>}/>
+            </Routes>
+          </HashRouter>
+        </Stack>
+      </HStack>
+
+    </ChakraProvider>
   );
 }
 
--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,10 +1,102 @@
-import './homePage.css'
+import { Heading, Box, Divider, Card, CardHeader, Text, CardBody, Icon, HStack} from "@chakra-ui/react"
+import {
+    MdOutlineDateRange,
+  } from 'react-icons/md';
+
+const ongoingTournament = [
+    {
+        name: 'Tuesday Tournament',
+        date: '07/03/2023'
+    }
+]
+
+const finishedTournament = [
+    {
+        name: 'Sunday Tournament',
+        date: '05/03/2023'
+    }
+]
+
+const createdTournament = [
+    {
+        name: 'Wednesday Tournament',
+        date: '08/03/2023'
+    }
+]
+
 export const HomePage = () =>{
     return(
-        <div>
-            <div className='title'>
-                <h1>Pairing System</h1>
-            </div>
-        </div>
+        <Box>
+            <Box>
+                <Heading mb={1} mt={1} fontSize='3xl'>
+                    Ongoing
+                </Heading>
+                <Divider mb={4}/>
+                {
+                    ongoingTournament.map((tournament)=>(
+                    <Card>
+                        <CardHeader px={2} py={1}>
+                            <Text fontSize='xl' as='b'>
+                                {tournament.name}
+                            </Text>
+                        </CardHeader>
+                        <CardBody px={2} py={0}>
+                            <HStack mb={2}>
+                                <Icon color="gray.500" as={MdOutlineDateRange} boxSize='6'/>
+                                <Text color="gray.400" fontSize='sm'> {tournament.date}</Text>
+                            </HStack>
+                        </CardBody>
+                    </Card>
+                    ))
+                }  
+            </Box>
+            <Box>
+                <Heading mb={1} mt={4} fontSize='3xl'>
+                    Created
+                </Heading>
+                <Divider mb={4}/>
+                {
+                    createdTournament.map((tournament)=>(
+                    <Card>
+                        <CardHeader px={2} py={1}>
+                            <Text fontSize='xl' as='b'>
+                                {tournament.name}
+                            </Text>
+                        </CardHeader>
+                        <CardBody px={2} py={0}>
+                            <HStack mb={2}>
+                                <Icon color="gray.500" as={MdOutlineDateRange} boxSize='6'/>
+                                <Text color="gray.400" fontSize='sm'> {tournament.date}</Text>
+                            </HStack>
+                        </CardBody>
+                    </Card>
+                    ))
+                }  
+            </Box>
+            <Box>
+                <Heading mb={1} mt={4} fontSize='3xl'>
+                    Finished
+                </Heading>
+                <Divider mb={4}/>
+                {
+                    finishedTournament.map((tournament)=>(
+                    <Card>
+                        <CardHeader px={2} py={1}>
+                            <Text fontSize='xl' as='b'>
+                                {tournament.name}
+                            </Text>
+                        </CardHeader>
+                        <CardBody px={2} py={0}>
+                            <HStack mb={2}>
+                                <Icon color="gray.500" as={MdOutlineDateRange} boxSize='6'/>
+                                <Text color="gray.400" fontSize='sm'> {tournament.date}</Text>
+                            </HStack>
+                        </CardBody>
+                    </Card>
+                    ))
+                }  
+            </Box>
+        </Box>
+        
     )
 }
